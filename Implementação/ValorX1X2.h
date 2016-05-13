4eb28a447b8aface0a8cb1471ca9443fef5e1687
Tiago Alves da Silva <tiagorts1996@hotmail.com>
2016-05-13 17:09:33 -0300

    Implememtação(Correção de Erro)

--- Implementação/ValorX1X2.h
+++ Implementação/ValorX1X2.h
@@ -3,7 +3,8 @@ class ValorX1X2{
 		float A, B, delt;
 	public:
 		ValorX1X2();
-		float Calcula_X1X2(float,float,float);
+		float Calcula_X1(float,float,float);
+		float Calcula_X2(float,float,float);
 
 
 };
